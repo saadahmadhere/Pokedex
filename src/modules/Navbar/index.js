343cda5baaf1e65a9350d1de95ec2716edb5608c
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-05-31 06:42:03 +0530

    [feat]: add navbar

---
+++ src/modules/Navbar/index.js
@@ -0,0 +1,11 @@
+import '../../styles/navbar.css';
+
+const Navbar = () => {
+	return (
+		<div className='nav'>
+			<span className='nav-logo'>Pokedex</span>
+		</div>
+	);
+};
+
+export default Navbar;
