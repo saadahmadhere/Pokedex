0216664b1a1516cb324eb300218ce98e3f37763c
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-06-01 08:54:35 +0530

    [feat]: set paths using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from './modules/Navbar';
 import MainFeed from './modules/MainFeed';
+import { Route, Routes } from 'react-router-dom';
+import Favourites from './screens/Favourites';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
-			<MainFeed />
+			<Routes>
+				<Route path='/' element={<MainFeed />} />
+				<Route path='/favourites' element={<Favourites />} />
+			</Routes>
 		</div>
 	);
 }
--- src/modules/Navbar/index.js
+++ src/modules/Navbar/index.js
@@ -1,15 +1,20 @@
+import { Link } from 'react-router-dom';
 import '../../styles/navbar.css';
 
 const Navbar = () => {
 	return (
 		<div className='nav'>
-			<span className='nav-logo'>Pokedex</span>
+			<Link to='/'>
+				<span className='nav-logo'>Pokedex</span>
+			</Link>
 			<input
 				type='text'
 				className='nav-search'
 				placeholder='Search by pokemon name or type'
 			/>
-			<span className='nav-fav'>Favourites</span>
+			<Link to='/favourites'>
+				<span className='nav-fav'>Favourites</span>
+			</Link>
 		</div>
 	);
 };
